60366a84a1e1fb2d8099518e5294ed0a844b72f0
Denzel Wamburu <denzjoseph@gmail.com>
2016-01-28 13:22:28 +0300

    add pay routes with post and get

--- server/boot/mpesa.js
+++ server/boot/mpesa.js
@@ -22,6 +22,87 @@ app.get('/payment_listener', PesaPal.paymentListener, function (req) {
         // TODO: Save in DB?
     }
 });
+
+//callback for paym
+app.get('/payment_callback', function (req, res) {
+    var options = { // Assumes pesapal calls back with a transaction id and reference
+        transaction: req.query[PesaPal.getQueryKey('transaction')],
+        reference: req.query[PesaPal.getQueryKey('reference')]
+    };
+
+    PesaPal.getPaymentDetails(options, function (error, payment) {
+        // check payment.status and proceed accordingly
+
+        var message = "Thank you for doing business with us.";
+        res.render("message", {
+            message: message,
+            details: JSON.stringify(payment, null, 2),
+            error: error ? JSON.stringify(error, null, 2) : null
+        });
+    });
+  });
+
+// route for Payment
+  app.get('/pay', function (req, res, next) {
+      // TODO: Render checkout UI
+      res.render("checkout", {
+          reference: new Date().getTime(),
+          description: "Pay Here",
+      //    amount: Math.floor((Math.random() * 20000) + 1)
+          amount: 50
+
+      });
+  });
+
+// payment route for post
+  app.post('/pay', function (req, res, next) {
+      // TODO: Make order from request;
+      var customer = new PesaPal.Customer(req.body.email, "");
+      customer.firstName = req.body.first_name;
+      customer.lastName = req.body.last_name;
+      var order = new PesaPal.Order(
+          req.body.reference,
+          customer,
+          req.body.description,
+          req.body.amount,
+          req.body.currency,
+          req.body.type);
+
+
+      if(req.body.pesapal) { // Redirect to PesaPal for payment
+
+          var paymentURI = PesaPal.getPaymentURL(order, "http://localhost:3000/payment_callback");
+          res.redirect(paymentURI);
+
+      } else { // Use Custom Payment Page
+
+          var mobilePayment = req.body.mobile != undefined;
+          var method = mobilePayment ? PesaPal.PaymentMethod.MPesa : PesaPal.PaymentMethod.Visa;
+
+          PesaPal.makeOrder(order, method, function (error, order) {
+
+              if(error) {
+                  res.send(error.message);
+              } else {
+
+                  // TODO: Save order in DB
+                  db.saveOrder(order);
+
+                  // TODO: Render UI to get mpesa transaction code or card details from user
+                  if (mobilePayment) {
+                      res.render("mobile", {
+                          reference: order.reference,
+                          instructions: "Send " + order.amount + " " + order.currency + " to " + method.account + " via " + method.name
+                      });
+                  } else {
+                      res.render("card", {reference: order.reference});
+                  }
+              }
+
+          });
+      }
+  });
+
 /** var options = {
     reference: "42314123", // Send this
     transaction: "175c6485-0948-4cb9-8d72-05a2c3f25be5" // or both.
