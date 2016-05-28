05ff252522ececc43857eec4d5b7823c325ab87a
Denzel Wamburu <denzjoseph@gmail.com>
2016-05-28 14:38:12 +0300

    Add lasRecievedId for when getting the sms

--- server/boot/sms.js
+++ server/boot/sms.js
@@ -12,7 +12,8 @@ module.exports = function(app) {
     keyword: '101',
     linkId: 'www.zusha.co.ke',
     to: "254704654445",
-    message: 'Server is Spinning'
+    message: 'Server is Spinning',
+    lastReceivedId: 2
   };
   console.log(opts);
 
@@ -38,8 +39,8 @@ module.exports = function(app) {
   sms.fetchMessages(opts)
     .then(function(s) {
       var jsoned = {};
-      jsoned = JSON.stringify(s);
-      console.log(jsoned);
+      // jsoned = JSON.stringify(s);
+      console.log(s.SMSMessageData.Messages[0].to);
     })
     .catch(function(error) {
       console.log(error);
